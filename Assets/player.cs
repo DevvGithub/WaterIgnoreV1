83db72a098c663dd9f4d16c7789dc4f1ee08c5c7
willijom <william.lindberg2@elev.ga.lbs.se>
2022-12-14 14:27:35 +0100

    lol

--- Assets/player.cs
+++ Assets/player.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class player : MonoBehaviour
 {
-    float time = 1;
     [SerializeField]
+    float time;
+    
     float O2timer;
+    [SerializeField]
+    TextMeshProUGUI o2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      O2timer = time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        time += Time.deltaTime;
-        if (time >= O2timer)
+        string timeString = time.ToString();
+        timeString = timeString.Split(',')[0];
+        o2.text = "02 "+timeString+" / "+O2timer;
+        time -= Time.deltaTime;
+        if (time <= 0)
         {
             Destroy(this.gameObject);
         }
@@ -26,7 +34,7 @@ public class player : MonoBehaviour
     {
         if (collision.gameObject.tag == "Respawn")
         {
-            time = 0;
+            time = O2timer;
         }
     }
        
