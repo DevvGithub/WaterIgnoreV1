ab0a73f4a44dc96e08fbd73a8d838c767397b965
willijom <william.lindberg2@elev.ga.lbs.se>
2022-12-14 13:35:07 +0100

    gg

---
+++ Assets/player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    float time = 1;
+    [SerializeField]
+    float O2timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= O2timer)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Respawn")
+        {
+
+        }
+    }
+       
+}
