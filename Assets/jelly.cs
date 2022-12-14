0887fdd1c985a8abeb937aec232b071d4fe51d21
Kemilcurry <emil.otterstrom@elev.ga.lbs.se>
2022-12-14 14:46:21 +0100

    jellyfish

--- Assets/jelly.cs
+++ Assets/jelly.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class jelly : MonoBehaviour
 {
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "bullet")
+        {
+            hitpoints += -1;
+        }
+    }
+    private int hitpoints = 2; 
     private float time = 0;
     private float timer = 2;
     void Start()
@@ -13,14 +21,17 @@ public class jelly : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0,-1,0) * Time.deltaTime;
-        time += Time.deltaTime;
-        if (time >= timer)
+        if(hitpoints <= 0)
         {
-            transform.position += new Vector3(0, 1, 0);
-            transform.position += new Vector3(0, 1, 0);
-
-            time = 0;
+            Destroy(this.gameObject);
         }
+        transform.position += new Vector3(0, -1, 0) * Time.deltaTime;
+        time += Time.deltaTime;
+        if (time >= 2)
+        {
+            transform.position += new Vector3(0, 2, 0);
+            time = 0;
+     }
+
     }
 }
