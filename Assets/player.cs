30222ad70685fda9015fa1603e7a132426d70f7d
willijom <william.lindberg2@elev.ga.lbs.se>
2022-12-14 15:07:57 +0100

    gg

--- Assets/player.cs
+++ Assets/player.cs
@@ -6,36 +6,43 @@ using TMPro;
 public class player : MonoBehaviour
 {
     [SerializeField]
-    float time;
+    float o2;
 
     float O2timer;
     [SerializeField]
-    TextMeshProUGUI TM;
+    TextMeshProUGUI o2TM;
+    [SerializeField]
+    TextMeshProUGUI HpTM;
+    [SerializeField]
+    int MaxHP;
+    int MaxHPsave;
 
     // Start is called before the first frame update
     void Start()
     {
-      O2timer = time;// så det står 10/10 O2
+      O2timer = o2;// så det står 10/10 O2
+      MaxHPsave = MaxHP;
     }
 
     // Update is called once per frame
     void Update()
     {
-        string timeString = time.ToString();
+        string timeString = 02.ToString();
         timeString = timeString.Split(',')[0];
         //så man inte ser 15 decimaler
-        TM.text = "O2: " + timeString+" / "+O2timer;//vad som ska stå på TM
-        time -= Time.deltaTime;// så att luften går ned över tid.
-        if (time <= 0)
+        o2TM.text = "O2: " + timeString+" / "+O2timer;//vad som ska stå på TM
+        o2 -= Time.deltaTime;// så att luften går ned över tid.
+        if (o2 <= 0)
         {
             Destroy(this.gameObject);
         }
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Respawn")//så O2 timmern resetas
         {
-            time = O2timer;
+            o2 = O2timer;
         }
         if (collision.gameObject.tag == "jellyfish")
         {
