9a7f2375a90720f173f69fa679b0125297dc209c
Kemilcurry <emil.otterstrom@elev.ga.lbs.se>
2022-12-20 09:04:27 +0100

    Update jelly.cs

--- Assets/jelly.cs
+++ Assets/jelly.cs
@@ -25,13 +25,19 @@ public class jelly : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
-        transform.position += new Vector3(0, -1, 0) * Time.deltaTime;
+        
         time += Time.deltaTime;
-        if (time >= 2)
+        if (time <= 2)
         {
-            transform.position += new Vector3(0, 2, 0);
+            transform.position += new Vector3(0, -1, 0) * Time.deltaTime;
+        }
+        if(time >= 2)
+        {
+            transform.position += new Vector3(0, 4, 0) * Time.deltaTime;
+        }
+        if(time >= 2.5)
+        {
             time = 0;
         }
-
     }
 }
